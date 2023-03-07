66743bb8b5650eeb82f44bf85e3f868360c8aac0
Tim Pietrusky <timpietrusky@gmail.com>
2023-03-07 08:24:24 +0100

    refactor(features): removed single plants, added firstRedraw, plantsAmount, specialPlantsAmount, environment

--- src/atoms/FxHashFeatures.js
+++ src/atoms/FxHashFeatures.js
@@ -1,26 +1,27 @@
 import { inRange } from "../utils/inRange";
 
 export function fxHashFeatures(props) {
-  const { plants, baseMs, redrawFactor, redrawInMs } = props;
+  const { plants, redrawInMs, plantsAmount, environment } = props;
 
-  const plantMissing = "nothing here yet"
-
-  window.$fxhashFeatures = {
-    // name: "nd-genuary32nd-plants-chatgpt3-001",
-    // artist: "NERDDISCO",
-    // web: "nerddis.co/nd-genuary32nd-plants-chatgpt3-001",
-    redrawInMs,
-    plant1: plantMissing,
-    plant2: plantMissing,
-    plant3: plantMissing,
-    plant4: plantMissing,
-    plant6: plantMissing,
-    plant7: plantMissing,
-    plant8: plantMissing,
-  };
+  let specialPlantsAmount = 0;
 
   for (let i = 0; i < plants.length; i++) {
-    window.$fxhashFeatures[`plant${i + 1}`] =
-      plants[i] !== undefined ? "<3" : plantMissing;
+    specialPlantsAmount += plants[i].isSpecial ? 1 : 0
   }
+
+  const firstRedraw = inRange({
+    currentValue: redrawInMs,
+    ranges: [
+      { fast: [0, 5000] },
+      { average: [5000, 15000] },
+      { cozy: [15000, 60000] },
+    ],
+  });
+
+  window.$fxhashFeatures = {
+    plantsAmount,
+    firstRedraw,
+    specialPlantsAmount,
+    environment
+  };
 }
