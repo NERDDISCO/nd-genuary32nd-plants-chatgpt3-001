ba7dc166d854cdb79f6e51171e2b8f703ae632f9
TimPietrusky <timpietrusky@gmail.com>
2023-03-15 11:17:20 +0100

    refactor(features): everything I love goes into redrawInMs

--- src/atoms/FxHashFeatures.js
+++ src/atoms/FxHashFeatures.js
@@ -6,15 +6,26 @@ export function fxHashFeatures(props) {
   let specialPlantsAmount = 0;
 
   for (let i = 0; i < plants.length; i++) {
-    specialPlantsAmount += plants[i].isSpecial ? 1 : 0
+    specialPlantsAmount += plants[i].isSpecial ? 1 : 0;
   }
 
+  // prettier-ignore
   const firstRedraw = inRange({
     currentValue: redrawInMs,
     ranges: [
-      { fast: [0, 5000] },
-      { average: [5000, 15000] },
-      { cozy: [15000, 60000] },
+      { tarantula:                                   [0, 250] },       // Pendulum - Tarantula
+      { itsATrap:                                    [250, 1000] },    // Star Wars
+      { schwifty:                                    [1000, 5000] },   // Rick & Morty
+      { thisIsTheWay:                                [5000, 15000] },  // Star Wars: The Mandalorian
+      { sAllGoodMan:                                 [15000, 20000] }, // Better Call Saul
+      { theresAlwaysMoneyInTheBananaStand:           [20000, 25000] }, // Arrested Development
+      { YouTurnYourselfAroundThatsWhatItsAllAbout:   [25000, 30000] }, // BoJack Horseman
+      { dangerZone:                                  [30000, 40000] }, // Archer
+      { goGodGo:                                     [40000, 42500] }, // South Park - Go God Go
+      { whatsMyAgeAgain:                             [42500, 50000] }, // Blink-182 - What's My Age Again
+      { sayMyName:                                   [50000, 55000] }, // Breaking Bad
+      { winterIsComing:                              [55000, 59999] }, // Game of Thrones
+      { weHaveDoneTheImpossibleAndThatMakesUsMighty: [59999, 60000] }  // Firefly
     ],
   });
 
@@ -22,6 +33,8 @@ export function fxHashFeatures(props) {
     plantsAmount,
     firstRedraw,
     specialPlantsAmount,
-    environment
+    environment,
   };
+
+  return window.$fxhashFeatures;
 }
